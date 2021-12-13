81a2985bf0e1f04d23ee69f12c1bce2810e3309a
Frityet <ambhogal01@gmail.com>
2021-12-13 08:06:52 -0800

    Header guard

--- src/common.hpp
+++ src/common.hpp
@@ -1,3 +1,6 @@
+#ifndef _WEB_SERVER_COMMON_
+#define _WEB_SERVER_COMMON_
+
 #if defined(__linux__) || defined(__linux) || defined(linux) || defined(__gnu_linux__)
     #define LINUX
 #elif defined(__APPLE__) && defined(__MACH__)
@@ -5,3 +8,5 @@
 #elif defined(_WIN32) || defined(_WIN64)
     #define WINDOWS
 #endif
+
+#endif //!_WEB_SERVER_COMMON_
